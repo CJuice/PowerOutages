551263673c88cc432bcc97abbcc691410e11ff96
CJuice <CJuice@users.noreply.github.com>
2018-09-28 15:09:40 -0400

    revisions

--- doit_PowerOutage_Main.py
+++ doit_PowerOutage_Main.py
@@ -4,7 +4,14 @@
 
 
 def main():
-    pass
+    import os
+    import PowerOutages_V2.doit_PowerOutage_DatabaseFunctionality
+    import PowerOutages_V2.doit_PowerOutage_ProviderClasses
+    import PowerOutages_V2.doit_PowerOutage_WebRelatedFunctionality
+
+    _root_project_path = os.path.dirname(__file__)
+    credentials_path = os.path.join(_root_project_path, "doit_PowerOutage_Credentials.cfg")
+    centralized_variables_path = os.path.join(_root_project_path, "doit_PowerOutage_CentralizedVariables.cfg")
 
 
 if __name__ == "__main__":
---
+++ doit_PowerOutage_ProviderClasses.py
@@ -0,0 +1,7 @@
+"""
+
+"""
+
+class Provider:
+    def __init__(self):
+        pass
