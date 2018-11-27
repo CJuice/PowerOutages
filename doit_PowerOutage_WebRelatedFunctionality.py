c07dcbbef56d16bc76052d7802fc39669699a47f
CJuice <CJuice@users.noreply.github.com>
2018-11-27 12:25:26 -0500

    documentation

--- doit_PowerOutage_WebRelatedFunctionality.py
+++ doit_PowerOutage_WebRelatedFunctionality.py
@@ -1,15 +1,26 @@
 """
-
+Module containing WebFunctionality class for web interaction
 """
 import requests
 
 
 class WebFunctionality:
-
+    """
+    Static class at time of implementation, contains make_web_request() for use by all providers in web transactions
+    """
     @staticmethod
     def make_web_request(uri, payload=None, style="GET", headers=None):
-        # NOTE: In requests module, pass dict directly using the json parameter and it will be encoded automatically
-        #   If use data= then must use json.dumps(payload) before passing it in
+        """
+        Static method available to all provider classes for use in making web requests
+        NOTE: In requests module, pass dict directly using the json parameter and it will be encoded automatically
+           If use data= then must use json.dumps(payload) before passing it in
+        :param uri: web path to which to make request
+        :param payload: payload, if present, to pass in request
+        :param style: style of the request, example: GET POST etc...
+        :param headers: request headers
+        :return: response, unless exception and then message returned
+        """
+
         try:
             return {"GET": requests.get(url=uri, params=payload),
                     "POST_data": requests.post(url=uri, data=payload, headers=headers),
