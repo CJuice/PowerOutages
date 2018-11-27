a1ccdafd54df96628d0acf1557c14566f2f47594
CJuice <CJuice@users.noreply.github.com>
2018-11-27 12:11:50 -0500

    documentation

--- doit_PowerOutage_PEPClasses.py
+++ doit_PowerOutage_PEPClasses.py
@@ -1,10 +1,16 @@
 """
-
+Module contains a PEP class that inherits from PEPDELParent class which inherits from Provider class.
+PEP class is an implementation specific to the peculiarities of the DEL feeds and the processing they require
+that is not common to all providers. PEP and DEL had shared functionality. PEPDELParent was created as a result and is
+intended to provide flexibility for future changes. It acts as an interface. PEP inherits from the PEPDELParent class.
 """
 from PowerOutages_V2.doit_PowerOutage_PEPDEL_ParentClass import PEPDELParent
 
 
 class PEP(PEPDELParent):
-
+    """
+    PEP specific functionality and variables for handling PEP feed data. Inherits from PEPDELParent and therefore
+    Provider.
+    """
     def __init__(self, provider_abbrev, style):
         super(PEP, self).__init__(provider_abbrev=provider_abbrev, style=style)
