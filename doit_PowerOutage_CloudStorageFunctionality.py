dd397cde511c22c5404b919417123e9985f0d9e1
CJuice <CJuice@users.noreply.github.com>
2020-11-10 13:11:11 -0500

    refactor: cleanup, documentation, alphabetical organization

--- doit_PowerOutage_CloudStorageFunctionality.py
+++ doit_PowerOutage_CloudStorageFunctionality.py
@@ -1,119 +1,119 @@
 """
-
+Class file for cloud storage of outage related data.
+Contains variables and functionality for processing and upserting data to our Socrata open data portal
 """
 
-import pandas as pd
 from PowerOutages_V2.doit_PowerOutage_UtilityClass import Utility as DOIT_UTIL
-import dataclasses
 from sodapy import Socrata
+import pandas as pd
+import dataclasses
+
 
 class CloudStorage:
     """
-
+    Variables and functionality for processing and cloud interactions of outage data.
     """
 
     def __init__(self, parser):
-        self.socrata_dt_string = None
-        self.master_outages_df = None
-        self.feed_status_df = None
-        self.outages_as_record_dicts_list = []
-        self.zipcode_outage_records_df = None
         self.county_outage_records_df = None
-        self.master_groupby_area = None
-        self.grouped_sums_df = None
-        self.zipcode_zipper = None
         self.county_zipper = None
+        self.feed_status_df = None
         self.feed_status_zipper = None
-        self.socrata_client = None
+        self.grouped_sums_df = None
+        self.master_groupby_area = None
+        self.master_outages_df = None
         self.opendata_apptoken = parser["OPENDATA"]["APPTOKEN"]
         self.opendata_domain = parser["OPENDATA"]["DOMAIN"]
         self.opendata_password = parser["OPENDATA"]["PASSWORD"]
         self.opendata_username = parser["OPENDATA"]["USERNAME"]
+        self.outages_as_record_dicts_list = []
+        self.socrata_client = None
+        self.socrata_dt_string = None
+        self.zipcode_outage_records_df = None
+        self.zipcode_zipper = None
 
-    def create_socrata_acceptable_dt_string(self):
-        """TODO
-        NOTE: Socrata requires a 'T' between date and time for string to be recognized. No spaces.
+    def calculate_county_outage_percentage(self) -> None:
         """
-        self.socrata_dt_string = DOIT_UTIL.current_date_time().replace(" ", "T")
+        Calculate the outage percentage for each county using the outage counts and customer counts, multiplied by 100
 
-    def isolate_zip_style_records(self):
-        """TODO"""
-        self.zipcode_outage_records_df = self.grouped_sums_df[self.grouped_sums_df["style"] == DOIT_UTIL.ZIP]
-
-    def isolate_county_style_records(self):
-        """TODO"""
-        self.county_outage_records_df = self.grouped_sums_df[self.grouped_sums_df["style"] == DOIT_UTIL.COUNTY]
-
-    def group_by_area(self):
-        """TODO"""
-        self.master_groupby_area = self.master_outages_df.groupby(by=["style", "area"], axis=0, as_index=False)
-
-    def sum_outages(self):
-        """TODO"""
-        self.grouped_sums_df = self.master_groupby_area.sum()
-
-    def create_unique_id_outages(self):
-        """TODO"""
-        self.grouped_sums_df["uid"] = self.grouped_sums_df["area"] + self.socrata_dt_string
-
-    def create_unique_id_feed_status(self):
-        """TODO"""
-        self.feed_status_df["uid"] = self.feed_status_df["prov_style"] + self.socrata_dt_string
-
-    def create_dt_stamp_column(self):
-        """TODO"""
-        self.grouped_sums_df["dt_stamp"] = self.socrata_dt_string
-
-    def calculate_county_outage_percentage(self):
-        """TODO"""
-        # SettingWithCopyWarning: A value is trying to be set on a copy of a slice from a DataFrame.
-        #   Try using .loc[row_indexer,col_indexer] = value instead. Still throws a SettingWithCopyWarning
+        Note: Encountered classic "SettingWithCopyWarning: A value is trying to be set on a copy of a slice from
+        a DataFrame. Try using .loc[row_indexer,col_indexer] = value instead." Revised functionality but still
+        throws a SettingWithCopyWarning. Tried multiple ways but did not succeed so used context manager to change
+        mode > chained assignment.
+        :return: None
+        """
         with pd.option_context('mode.chained_assignment', None):
             print(pd.options.mode.chained_assignment)
             print("NOTE: Pandas mode.chained_assignment set to None to handle SettingWithCopyWarning in calculate_county_outage_percentage")
             self.county_outage_records_df["percent_out"] = self.county_outage_records_df.loc[:, "outages"].copy() / self.county_outage_records_df.loc[:, "customers"].copy() * 100
             # self.county_outage_records_df["percent_out"] = self.county_outage_records_df.apply(func=lambda row: row["outages"] / row["customers"] * 100, axis=1, raw=False, result_type=None)
+        return None
 
-    def create_outage_records(self, provider_objects: dict):
-        """TODO"""
-        for key, obj in provider_objects.items():
-            self.outages_as_record_dicts_list.extend([dataclasses.asdict(stat_obj) for stat_obj in obj.stats_objects])
-
-    def create_master_outage_dataframe(self):
-        """TODO"""
-        self.master_outages_df = pd.DataFrame.from_records(data=self.outages_as_record_dicts_list,
-                                                           columns=["style", "area", "outages", "customers"])
-
-    def create_feed_status_dataframe(self, status_check_output: dict):
-        """TODO
+    def correct_status_created_dt(self) -> None:
         """
-        self.feed_status_df = pd.DataFrame(data=status_check_output).transpose().reset_index().rename(columns={"index": "prov_style"})
-
-    def correct_status_created_dt(self):
-        """
-        TODO
-        :return:
+        Replace space with 'T' so that Socrata will accept/recognize the datetime values as that type instead of text
+        :return: None
         """
         self.feed_status_df["created"] = self.feed_status_df["created"].apply(lambda x: x.replace(" ", "T"))
+        return None
 
-    def drop_customers_from_zip_df(self):
-        """TODO"""
-        self.zipcode_outage_records_df.drop(columns=["customers"], inplace=True)
-
-    def drop_style_from_record_dfs(self):
-        """TODO"""
-        drop_field = "style"
-        self.zipcode_outage_records_df.drop(columns=[drop_field], inplace=True)
-        self.county_outage_records_df.drop(columns=[drop_field], inplace=True)
-
-    def create_lists_of_record_dicts(self):
+    def create_dt_stamp_column(self) -> None:
         """
+        Create and set the datetime stamp column to the Socrata acceptable value
+        :return: None
+        """
+        self.grouped_sums_df["dt_stamp"] = self.socrata_dt_string
+        return None
 
-        :return:
+    def create_feed_status_dataframe(self, status_check_output: dict) -> None:
+        """
+        Create a pandas dataframe from a dict of provider feed status checks.
+        Function creates dataframe, transposes data, resets index, and renames column before returning.
+
+        :param status_check_output: dict of feed status check information
+        :return: None
+        """
+        self.feed_status_df = pd.DataFrame(data=status_check_output).transpose().reset_index().rename(columns={"index": "prov_style"})
+        return None
+
+    def create_lists_of_record_dicts(self) -> None:
+        """
+        Create list of record data from record dataframes for upsert payload to socrata open data portal.
+        Performs the action on the county, zipcode, and feed status dataframes in one call.
+        :return: None
         """
         self.county_zipper = self.county_outage_records_df.to_dict(orient="records")
         self.zipcode_zipper = self.zipcode_outage_records_df.to_dict(orient="records")
         self.feed_status_zipper = self.feed_status_df.to_dict(orient="records")
+        return None
+
+    def create_master_outage_dataframe(self) -> None:
+        """
+        Create pandas dataframe from list of outage dicts
+        :return: None
+        """
+        self.master_outages_df = pd.DataFrame.from_records(data=self.outages_as_record_dicts_list,
+                                                           columns=["style", "area", "outages", "customers"])
+        return None
+
+    def create_outage_records(self, provider_objects: dict) -> None:
+        """
+        Create a list of outage record dicts from the provider objects
+        :param provider_objects: dict of provider objects
+        :return: None
+        """
+        for obj in provider_objects.values():
+            self.outages_as_record_dicts_list.extend([dataclasses.asdict(stat_obj) for stat_obj in obj.stats_objects])
+        return None
+
+    def create_socrata_acceptable_dt_string(self) -> None:
+        """
+        Create a socrata acceptable datetime string by replacing space with 'T'
+        NOTE: Socrata requires a 'T' between date and time for string to be recognized. No spaces.
+        :return: None
+        """
+        self.socrata_dt_string = DOIT_UTIL.current_date_time().replace(" ", "T")
+        return None
 
     def create_socrata_client(self) -> None:
         """
@@ -127,6 +127,74 @@ class CloudStorage:
         """
         self.socrata_client = Socrata(domain=self.opendata_domain, app_token=self.opendata_apptoken,
                                       username=self.opendata_username, password=self.opendata_password)
+        return None
+
+    def create_unique_id_feed_status(self) -> None:
+        """
+        Combine provider style key with socrata acceptable datetime string to make a record unique id for feed status
+        :return: None
+        """
+        self.feed_status_df["uid"] = self.feed_status_df["prov_style"] + self.socrata_dt_string
+        return None
+
+    def create_unique_id_outages(self) -> None:
+        """
+        Combine area value with socrata acceptable datetime string to make a record unique id for group sums
+
+        :return:
+        """
+        self.grouped_sums_df["uid"] = self.grouped_sums_df["area"] + self.socrata_dt_string
+
+    def drop_customers_from_zip_df(self) -> None:
+        """
+        Drop the 'customers' field from the zip code dataframe.
+        Customer count values are not reported by all providers for zip code areas so can't calculate across all areas
+        like is done in the county data.
+        :return: None
+        """
+        self.zipcode_outage_records_df.drop(columns=["customers"], inplace=True)
+        return None
+
+    def drop_style_from_record_dfs(self) -> None:
+        """
+        Drop the 'style' field from the zip code, and county dataframes
+        :return: None
+        """
+        drop_field = "style"
+        self.zipcode_outage_records_df.drop(columns=[drop_field], inplace=True)
+        self.county_outage_records_df.drop(columns=[drop_field], inplace=True)
+        return None
+
+    def isolate_county_style_records(self) -> None:
+        """
+        Isolate the county style records from the master outage dataframe
+        :return: None
+        """
+        self.county_outage_records_df = self.grouped_sums_df[self.grouped_sums_df["style"] == DOIT_UTIL.COUNTY]
+        return None
+
+    def isolate_zip_style_records(self) -> None:
+        """
+        Isolate the zip style records from the master outage dataframe
+        :return: None
+        """
+        self.zipcode_outage_records_df = self.grouped_sums_df[self.grouped_sums_df["style"] == DOIT_UTIL.ZIP]
+        return None
+
+    def group_by_area(self) -> None:
+        """
+        Group the master dataframe by style and area
+        :return: None
+        """
+        self.master_groupby_area = self.master_outages_df.groupby(by=["style", "area"], axis=0, as_index=False)
+        return None
+
+    def sum_outages(self) -> None:
+        """
+        Operate on the master groupby and sum the outage data for each group
+        :return: None
+        """
+        self.grouped_sums_df = self.master_groupby_area.sum()
         return None
 
     def upsert_to_socrata(self, dataset_identifier: str, zipper: dict) -> None:
